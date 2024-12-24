07f03dcdf9fbd612474e0e5fa78cc6c5a5dfd607
fjmasis <102482854+fjmasis@users.noreply.github.com>
2024-12-23 20:45:20 -0600

    se agregaron las clases y la clase llamada contacto.cs ademas en el app code la clase databaseHelper para conectar con la base de datos.Ademas el folder pages con web form agregar contactar

---
+++ FranciscoMasisAgregarContactos/App_Code/DatabaseHelper.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace FranciscoMasisAgregarContactos.App_Code
+{
+    public class DatabaseHelper
+    {
+        private string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["FranciscoMasisAgregarContactos"].ConnectionString;
+        }
+
+        public void LogError(Exception ex)
+        {
+            string logFilePath = @"C:\WebP\errorFranciscoMasisAgregarContactos.txt";
+
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                writer.WriteLine($"{DateTime.Now} - Error: {ex.Message}");
+                writer.WriteLine($"StackTrace: {ex.StackTrace}");
+                writer.WriteLine();
+            }
+        }
+
+
+        public DataTable ExecuteSelectQuery(string query, SqlParameter[] sqlParameters)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(GetConnectionString()))
+                {
+                    conn.Open();
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        if (sqlParameters != null)
+                        {
+                            cmd.CommandType = CommandType.StoredProcedure;
+                            cmd.Parameters.AddRange(sqlParameters);
+                        }
+
+                        using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                        {
+                            DataTable dt = new DataTable();
+                            adapter.Fill(dt);
+                            return dt;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogError(ex);
+                throw;
+            }
+        }
+
+
+        public void ExecuteQuery(string query, SqlParameter[] sqlParameters)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(GetConnectionString()))
+                {
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddRange(sqlParameters);
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogError(ex);
+                throw;
+            }
+        }
+
+        internal int ExecuteNonQuery(string query, SqlParameter[] sqlParameters)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(GetConnectionString()))
+                {
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        if (sqlParameters != null)
+                        {
+                            cmd.Parameters.AddRange(sqlParameters);
+                        }
+                        conn.Open();
+                        return cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogError(ex);
+                throw;
+            }
+        }
+
+
+    }
+}
---
+++ FranciscoMasisAgregarContactos/Pages/AgregarContacto.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AgregarContacto.aspx.cs" Inherits="FranciscoMasisAgregarContactos.Pages.AgregarContacto" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Agregar Contacto</title>
+</head>
+<body>
+    <h1 id="formHeader">Agregar Contacto</h1>
+    <form id="formAgregarContacto" runat="server">
+        <div class="form-input">
+            <asp:Label Text="Nombre:" AssociatedControlID="txtNombre" runat="server" />
+            <asp:TextBox ID="txtNombre" runat="server" />
+        </div>
+        <div class="form-input">
+            <asp:Label Text="Apellido:" AssociatedControlID="txtApellido" runat="server" />
+            <asp:TextBox ID="txtApellido" runat="server" />
+        </div>
+        <div class="form-input">
+            <asp:Label Text="Teléfono:" AssociatedControlID="txtTelefono" runat="server" />
+            <asp:TextBox ID="txtTelefono" runat="server" />
+        </div>
+        <div class="form-input">
+            <asp:Label Text="Email:" AssociatedControlID="txtEmail" runat="server" />
+            <asp:TextBox ID="txtEmail" runat="server" />
+            <asp:RegularExpressionValidator
+                ControlToValidate="txtEmail"
+                ErrorMessage="Email inválido"
+                ValidationExpression="\w+@\w+\.\w+"
+                runat="server" />
+        </div>
+        <asp:Button Text="Agregar" CssClass="btn-submit" OnClick="AgregarContacto_Click" runat="server" />
+    </form>
+</body>
+</html>
